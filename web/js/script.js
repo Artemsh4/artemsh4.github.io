3d626383be40ab138a9642c3a0dd7bcc0ef82d38
Artemsh4 <113462768+Artemsh4@users.noreply.github.com>
2022-09-14 15:11:53 +0300

    Add files via upload

--- web/js/script.js
+++ web/js/script.js
@@ -23,11 +23,14 @@ btn.addEventListener('click', (e) => {
         btn.classList.remove('change');
     }
 });
+var modal = document.getElementById('id01');
 
-
-
-
-
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 let dropdown = document.getElementsByClassName("dropdown-btn");
 let i;
